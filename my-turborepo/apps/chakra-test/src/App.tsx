52c7f69b04134ee3831b7afe2e8cec3ed2dbd7de
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2024-01-25 11:55:50 +0000

    got a chakra animation to work

--- my-turborepo/apps/chakra-test/src/App.tsx
+++ my-turborepo/apps/chakra-test/src/App.tsx
@@ -3,6 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { tomography } from '@repo/science'
+import BoxTest from './ChakraBox'
+
+
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -31,6 +35,7 @@ function App() {
         Click on the Vite and React logos to learn more
         from tomography {m}
       </p>
+      <BoxTest/>
     </>
   )
 }

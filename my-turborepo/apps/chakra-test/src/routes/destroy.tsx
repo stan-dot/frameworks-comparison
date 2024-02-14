bbbf39445f3531da761199ac43abcdf848c36565
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2024-02-14 15:33:32 +0000

    remove daq ui, move layout stuff

--- my-turborepo/apps/chakra-test/src/routes/destroy.tsx
+++ my-turborepo/apps/chakra-test/src/routes/destroy.tsx
@@ -1,13 +1,12 @@
-import { Form, redirect, useLoaderData } from "react-router-dom";
+import { Form, redirect } from "react-router-dom";
 import { deleteContact } from "../contacts";
 
-export async function action({ request, params }) {
+export async function action({ params }) {
   await deleteContact(params.contactId);
   return redirect(`/`);
 }
 
 export default function DeleteContact() {
-  const { contact } = useLoaderData();
 
   return (
     <Form method="post" id="delete-contact-form">

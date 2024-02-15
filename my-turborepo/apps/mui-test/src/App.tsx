82b3b0fb812d179e7c83535c78edfca331b3cc88
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2024-02-15 10:08:14 +0000

    make mui test part 1
    undefined

--- my-turborepo/apps/mui-test/src/App.tsx
+++ my-turborepo/apps/mui-test/src/App.tsx
@@ -1,40 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { tomography } from '@repo/science';
-import './App.css'
-import { Button } from '@repo/ui/button';
+import Box from "@mui/material/Box";
+import { tomography } from "@repo/science";
+import { Button } from "@repo/ui/button";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
   const m = tomography();
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div style={{ width: "100%" }}>
+        <Box
+          sx={{
+            display: "grid",
+            bgcolor: (theme) =>
+              theme.palette.mode === "dark" ? "#101010" : "grey.100",
+            color: (theme) =>
+              theme.palette.mode === "dark" ? "grey.300" : "grey.800",
+            border: "1px solid",
+            borderColor: (theme) =>
+              theme.palette.mode === "dark" ? "grey.800" : "grey.300",
+            p: 1,
+            borderRadius: 2,
+            fontSize: "0.875rem",
+            fontWeight: "700",
+          }}
+        >
+          {"I'm a grid container!"}
+        </Box>
+        <Box
+          sx={{
+            display: "grid",
+            gridTemplateColumns: "repeat(4, 1fr)",
+            gap: 1,
+            gridTemplateRows: "auto",
+            gridTemplateAreas: `"header header header header"
+  "main main . sidebar"
+  "footer footer footer footer"`,
+          }}
+        >
+          <Box sx={{ gridArea: "header", bgcolor: "primary.main" }}>Header</Box>
+          <Box sx={{ gridArea: "main", bgcolor: "secondary.main" }}>Main</Box>
+          <Box sx={{ gridArea: "sidebar", bgcolor: "error.main" }}>Sidebar</Box>
+          <Box sx={{ gridArea: "footer", bgcolor: "warning.dark" }}>Footer</Box>
+        </Box>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <h5> hello from the subtitle</h5>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <Button appName={'vite'}><p>nothing, but from tomography we get: {m}</p></Button>
+      <Button appName={"vite"}>
+        <p>nothing, but from tomography we get: {m}</p>
+      </Button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

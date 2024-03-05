2c940b1d6b1bd8c2f0381a751ce95cddf9e7c590
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2024-03-05 14:59:55 +0000

    set up deployments for package comparator

---
+++ my-turborepo/deployments/prepare.sh
@@ -0,0 +1,19 @@
+
+tmp_location="package-comparator"
+# todo assign which registry - private or public
+# todo consider creating the dockerfile dynamically from a template
+
+# based on this https://confluence.diamond.ac.uk/pages/viewpage.action?spaceKey=CLOUD&title=Container+Registry
+module load argus
+gcloud auth login 
+gcloud auth configure-docker
+dockerfile_path="deployments/Dockefile.$tmp_location"
+podman build --format docker 
+new_tag="gcr.io/diamond-privreg/$tmp_location:latest"
+podman tag $tmp_location:latest $new_tag
+
+podman images
+
+podman push $new_tag
+
+kubectl apply -f deployments/$tmp_location.yaml
